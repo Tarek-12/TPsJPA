fe2348773e48f1116fcaf1c69eb92e8a64ba1787
Tarek <tarekhassouni5@gmail.com>
2022-03-11 17:39:15 +0100

    c14

--- src/main/java/ma/cigma/FrontEndApplication.java
+++ src/main/java/ma/cigma/FrontEndApplication.java
@@ -3,6 +3,8 @@ package ma.cigma;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -14,4 +16,6 @@ public class FrontEndApplication {
 
 	@Bean
 	public RestTemplate getRestTemplate() { return new RestTemplate(); }
+	@Bean
+	public PasswordEncoder getPassword() { return new BCryptPasswordEncoder(); }
 }
